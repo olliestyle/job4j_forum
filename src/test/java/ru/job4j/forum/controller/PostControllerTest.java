33307a0e44e2179b45a6dd17aaf521bf36d563d4
olliestyle <olliestyle1989@gmail.com>
2022-04-01 15:09:38 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/controller/PostControllerTest.java
+++ src/test/java/ru/job4j/forum/controller/PostControllerTest.java
@@ -1,14 +1,22 @@
 package ru.job4j.forum.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,6 +26,39 @@ public class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @Test
+    @WithMockUser
+    public void whenCreatePost() throws Exception {
+        this.mockMvc.perform(post("/create")
+        .param("name", "куплю ладу")
+        .param("description", "Очень дорого!"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(postService).create(argument.capture());
+        assertThat(argument.getValue().getName(), is("куплю ладу"));
+        assertThat(argument.getValue().getDescription(), is("Очень дорого!"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenEditPost() throws Exception {
+        this.mockMvc.perform(post("/edit")
+                .param("name", "куплю иномарку")
+                .param("description", "Недорого"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(postService).edit(argument.capture());
+        assertThat(argument.getValue().getName(), is("куплю иномарку"));
+        assertThat(argument.getValue().getDescription(), is("Недорого"));
+    }
 
     @Test
     @WithMockUser
--- src/test/java/ru/job4j/forum/controller/RegControllerTest.java
+++ src/test/java/ru/job4j/forum/controller/RegControllerTest.java
@@ -1,17 +1,29 @@
 package ru.job4j.forum.controller;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.service.PostService;
+import ru.job4j.forum.service.RegistrationService;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -20,11 +32,30 @@ public class RegControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private RegistrationService registrationService;
+
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/reg"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
+    }
+
+    @Test
+    public void shouldRegisterUser() throws Exception {
+        this.mockMvc.perform(post("/reg")
+            .param("username", "default")
+            .param("email", "default@mail.ru")
+            .param("password", "default"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(registrationService).saveUser(argument.capture());
+        assertThat(argument.getValue().getUsername(), is("default"));
+        assertThat(argument.getValue().getEmail(), is("default@mail.ru"));
+        assertTrue(new BCryptPasswordEncoder().matches("default", argument.getValue().getPassword()));
     }
 }
